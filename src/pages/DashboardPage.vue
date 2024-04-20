7c9ac70e96fc4146df9062ac24368d0c6883a299
Yugank Singh <yuganksingh05@gmail.com>
2024-04-20 16:35:58 +0530

    add dashboard overview

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -56,14 +56,14 @@
 						</span>
 						{{ " " }}
 						<span class="detail-change up" v-if="details.change > 0">
-							<svg width="20" height="10">
+							<svg width="20" height="10" fill="#0f77fa">
 								<polygon points="1,10 19,10 10,0" />
 							</svg>
 
 							{{ (details.change * 100).toFixed(2) }} %
 						</span>
 						<span class="detail-change down" v-if="details.change < 0">
-							<svg width="20" height="10">
+							<svg width="20" height="10" fill="#ff4b75">
 								<polygon points="1,0 19,0 10,10" />
 							</svg>
 							{{ (details.change * -100).toFixed(2) }} %
@@ -80,6 +80,63 @@
 							:key="key"
 						>
 							<img :alt="key" :src="details.imageUrl" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="details-countries">
+				<div
+					class="countries-container"
+					v-for="(details, key) in countries"
+					:key="key"
+				>
+					<div class="name">{{ details.name }}</div>
+					<div class="points">
+						<p>{{ details.points }}</p>
+						<svg
+							width="20"
+							height="10"
+							fill="#0f77fa"
+							v-if="details.position === 'up'"
+						>
+							<polygon points="1,10 19,10 10,0" />
+						</svg>
+
+						<svg
+							width="20"
+							height="10"
+							fill="#ff4b75"
+							v-if="details.position === 'down'"
+						>
+							<polygon points="1,0 19,0 10,10" />
+						</svg>
+					</div>
+
+					<svg width="100" height="50">
+						<!-- Line -->
+						<path
+							d="M 10 40 L 30 20 L 50 40 L 70 20 L 90 45 L 95 20"
+							fill="none"
+							stroke="blue"
+							stroke-width="1"
+						/>
+					</svg>
+				</div>
+			</div>
+			.
+			<div class="team-members">
+				<div class="top">
+					<h3 class="heading">Team Members</h3>
+					<button class="view-all">View All</button>
+				</div>
+				<div class="members-wrapper">
+					<div class="member" v-for="(member, key) in teamMembers" :key="key">
+						<div class="left">
+							<img :src="member.imageUrl" :alt="member.name" />
+							<p>{{ member.name }}</p>
+						</div>
+						<div class="right">
+							<div class="position">{{ member.position }}</div>
 						</div>
 					</div>
 				</div>
@@ -112,6 +169,36 @@ export default {
 				EUR: { imageUrl: "/public-images/currency-euro.svg" },
 				YEN: { imageUrl: "/public-images/currency-yen.svg" },
 				BTC: { imageUrl: "/public-images/bitcoin-2.svg" },
+			},
+			countries: {
+				italy: { name: "Italy", points: 482, position: "up" },
+				us: { name: "United States", points: 12, position: "down" },
+				canada: { name: "Canada", points: 244, position: "up" },
+				spain: { name: "Spain", points: 241, position: "down" },
+				japan: { name: "Japan", points: 980, position: "up" },
+				sa: { name: "South Africa", points: 391, position: "down" },
+			},
+			teamMembers: {
+				lkjasd: {
+					name: "Andre Johnson",
+					imageUrl: "/public-images/profile-image-1.jpg",
+					position: "Top Trader",
+				},
+				asdf: {
+					name: "Anna Atkinsons",
+					imageUrl: "/public-images/profile-image-2.jpg",
+					position: "Manager",
+				},
+				qwer: {
+					name: "Mark Atkinsons",
+					imageUrl: "/public-images/profile-image-3.jpg",
+					position: "Manager",
+				},
+				opidf: {
+					name: "Johanna Tairons",
+					imageUrl: "/public-images/profile-image-4.jpg",
+					position: "Designer",
+				},
 			},
 			selectedCurrency: "$",
 		}
@@ -290,6 +377,100 @@ main {
 						}
 					}
 					img {
+					}
+				}
+			}
+		}
+		.details-countries {
+			display: flex;
+			flex-direction: row;
+			padding: 20px 60px;
+			background-color: white;
+			box-shadow: 0px 3px 26px #00000017;
+			justify-content: space-evenly;
+			column-gap: 30px;
+			flex-wrap: wrap;
+			margin-bottom: 30px;
+			.countries-container {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				padding: 30px 10px;
+				.name {
+					color: #495172;
+					font-size: 12px;
+					margin-bottom: 20px;
+				}
+				.points {
+					display: flex;
+					flex-direction: row;
+					align-items: center;
+					font-size: 30px;
+					column-gap: 10px;
+					p {
+						margin: 0;
+					}
+				}
+			}
+		}
+		.team-members {
+			width: 50%;
+			.top {
+				display: flex;
+				flex-direction: row;
+				justify-content: space-between;
+				align-items: flex-end;
+				margin: 30px 0;
+				.heading {
+					font-weight: 300;
+					font-size: 25px;
+					padding: 0;
+					margin: 0;
+				}
+				.view-all {
+					all: unset;
+					font-size: 12px;
+					color: #2464fc;
+					cursor: pointer;
+				}
+			}
+			.members-wrapper {
+				display: flex;
+				flex-direction: column;
+				row-gap: 10px;
+				.member {
+					display: flex;
+					flex-direction: row;
+					justify-content: space-between;
+					align-items: center;
+
+					padding: 15px 25px;
+					background-color: white;
+					box-shadow: 0px 3px 26px #00000017;
+					.left {
+						display: flex;
+						flex-direction: row;
+						column-gap: 18px;
+						align-items: center;
+						img {
+							height: 48px;
+							width: 48px;
+							border-radius: 24px;
+							object-fit: cover;
+						}
+						p {
+							margin: 0;
+							font-size: 18px;
+						}
+					}
+					.right {
+						.position {
+							font-size: 12px;
+							color: #0f77fa;
+							background-color: #ecf2fe;
+							padding: 10px 20px;
+							border-radius: 30px;
+						}
 					}
 				}
 			}
